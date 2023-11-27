7ae8ec2ed2c84ea9038aef4d6a5ff8553bcd3d57
Gibbon Ho <gibbon2000pro@qq.com>
2023-11-27 20:54:32 +0800

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   CMakeLists.txt
    	new file:   README.md
    	new file:   include/gil.h

---
+++ README.md
@@ -0,0 +1,3 @@
+Gibbon's Interpreted Language
+
+借鉴lil项目 http://runtimeterror.com/tech/lil/
---
+++ include/gil.h
@@ -0,0 +1,12 @@
+#ifndef _GIBBON_INTERPRETED_LANGUAGE_H_
+#define _GIBBON_INTERPRETED_LANGUAGE_H_
+
+#include <memory>
+
+namespace gil {
+
+    
+
+}  // namespace gil
+
+#endif  //_GIBBON_INTERPRETED_LANGUAGE_H_

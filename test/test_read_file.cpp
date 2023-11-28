3029684927748796a58fef9cee753013221fa7cb
Gibbon Ho <gibbon2000pro@qq.com>
2023-11-29 00:00:35 +0800

    位于分支 gibbon-dev  您的分支与上游分支 'origin/gibbon-dev' 一致。
     要提交的变更：
    	修改：     CMakeLists.txt
    	修改：     test/test_read_file.cpp

--- test/test_read_file.cpp
+++ test/test_read_file.cpp
@@ -1,5 +1,7 @@
+#include <codecvt>
 #include <fstream>
 #include <iostream>
+#include <locale>
 #include <string>
 
 void ReadFile(const std::string& name) {
@@ -7,17 +9,43 @@ void ReadFile(const std::string& name) {
     if (!file) {
         return;
     }
+
+    std::wstring_convert<std::codecvt_utf8<wchar_t>> conv;
+    std::setlocale(LC_ALL, "zh_CN.UTF-8");
+    // file.imbue(std::locale("zh_CN.UTF-8"));
+
+    std::wcout << L"贺渊凌" << std::endl;
+
+    while (!file.eof()) {
+        std::string line;
+        std::getline(file, line);
+        // std::cout << line << std::endl;
+        // for (auto& c : line) {
+        //     std::cout << c << std::endl;
+        // }
+        std::wstring wline = conv.from_bytes(line);
+        std::wcout << wline.length() << std::endl;
+        std::wcout << wline << std::endl;
+        // wprintf(L"%ls\n", wline);
+        for (auto& c : wline) {
+            std::wcout << c << std::endl;
+        }
+    }
+
+    file.close();
 }
 
 int main(int argc, char* argv[]) {
     if (argc > 0) {
         std::string arg0(argv[0]);
-        std::cout << "arg0:" << arg0 << std::endl;
+        // std::cout << "arg0:" << arg0 << std::endl;
     }
 
     if (argc > 1) {
         std::string arg1(argv[1]);
-        std::cout << "arg1:" << arg1 << std::endl;
+        // std::cout << "arg1:" << arg1 << std::endl;
+
+        ReadFile(arg1);
     }
 
     return 0;
